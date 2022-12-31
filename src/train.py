77ed3a56d3a4e2cca990ccce563b03b3ba10d525
aryankhatana01 <aryankhatana01@gmail.com>
2022-12-31 15:56:23 +0530

    improved using tez

--- src/train.py
+++ src/train.py
@@ -7,6 +7,22 @@ from sklearn import model_selection, preprocessing
 from dataset import MovieDataset
 from model import RecSysModel
 
+def train_one_epoch(epoch, model, optimizer, scheduler, data_loader, device):
+    model.train()
+    final_loss = 0
+    for data in data_loader:
+        user_id = data['user_id'].to(device)
+        movie_id = data['movie_id'].to(device)
+        rating = data['rating'].to(device)
+        optimizer.zero_grad()
+        output = model(user_id, movie_id)
+        loss = F.mse_loss(output, rating.view(-1, 1))
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        final_loss += loss.item()
+    print(f'Epoch={epoch}, Loss={final_loss/len(data_loader)}')
+
 def train():
     df = pd.read_csv('../input/ratingsCleaned.csv') # UserId, MovieId, Rating
     # Preprocess the data
@@ -43,21 +59,51 @@ def train():
         n_movies=len(lbl_movies.classes_)
     )
 
-    # Creating DataLoader
-    train_loader = DataLoader(
-        train_ds,
-        batch_size=1024,
-        num_workers=2,
-        shuffle=True,
-        pin_memory=True
+    # print(model)
+    model.fit(
+        train_ds, 
+        test_ds, 
+        epochs=10, 
+        train_bs=1024, 
+        valid_bs=1024, 
+        fp16=True,
+        pin_memory=False
     )
 
-    valid_loader = DataLoader(
-        test_ds,
-        batch_size=1024,
-        num_workers=2,
-        pin_memory=True
-    )
+    # # Creating DataLoader
+    # train_loader = DataLoader(
+    #     train_ds,
+    #     batch_size=1024,
+    #     num_workers=2,
+    #     shuffle=True,
+    #     pin_memory=True
+    # )
+
+    # valid_loader = DataLoader(
+    #     test_ds,
+    #     batch_size=1024,
+    #     num_workers=2,
+    #     pin_memory=True
+    # )
+
+    # # Defining the device
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # model.to(device)
+
+    # # Defining the optimizer
+    # optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    # # Defining the scheduler
+    # scheduler = torch.optim.lr_scheduler.OneCycleLR(
+    #     optimizer=optimizer,
+    #     max_lr=1e-3,
+    #     steps_per_epoch=len(train_loader),
+    #     epochs=10
+    # )
+
+    # # Training the model
+    # for epoch in range(10):
+    #     train_one_epoch(epoch, model, optimizer, scheduler, train_loader, device)
 
 if __name__ == "__main__":
     train()
