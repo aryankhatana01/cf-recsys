e5973f28c20ac8e7cc8e99ffe27e2004b422f45d
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-01 01:30:18 +0530

    added docstrings + updated requirements.txt

--- api/api.py
+++ api/api.py
@@ -1,3 +1,10 @@
+"""
+FastAPI is a modern, fast (high-performance), web framework for building APIs with Python 3.6+ based on standard Python type hints.
+
+To Run the Server:
+uvicorn api:app --reload
+"""
+
 from typing import Union
 
 from fastapi import FastAPI
