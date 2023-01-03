dcf1b236474203726954c405331f7a66f261f348
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-03 16:42:08 +0530

    added a function to retrieve titles from predicted recommendations

--- turimodel/predictRecommendations.py
+++ turimodel/predictRecommendations.py
@@ -24,12 +24,27 @@ def create_new_user_ratings(movieIds: list, ratings: list, userIds: list):
 
 # Creating a new user with their ratings
 new_user_ratings = create_new_user_ratings(
-    movieIds=[1, 74, 52, 44, 200], 
-    ratings=[5, 4, 3, 2, 1], 
-    userIds=[5000000, 5000000, 5000000, 5000000, 5000000]
+    movieIds=[1073, 919, 1097], 
+    ratings=[5, 5, 5], 
+    userIds=[5000000, 5000000, 5000000]
 )
 
 # Making recommendations
 recommendations = model.recommend([5000000], new_observation_data=new_user_ratings)
 
-print(recommendations)
+# Converting the recommendations to a list
+list_of_recommandations = list(recommendations['movieId'])
+
+# A function to convert the list of recommendations to their corresponding titles
+def convertRecommendationsToTitle(list_of_recommandations):
+    df = pd.read_csv('../input/movie.csv')
+    df = df[['movieId', 'title', 'genres']]
+    df = df.set_index('movieId')
+    df = df.loc[list_of_recommandations]
+    return df
+
+# Converting the recommendations to their corresponding titles
+df = convertRecommendationsToTitle(list_of_recommandations)
+
+# Printing the top 10 recommendation's titles
+print(df.head(10))
