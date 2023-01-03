689c983cd86b7595347aa0eaefa49797c6978069
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-03 23:23:53 +0530

    recommend endpoint now returning the titls of recommened movies

--- api/api.py
+++ api/api.py
@@ -27,8 +27,9 @@ def read_root():
 def create_pred(movieIds: List[int] = Query(None), ratings: List[int] = Query(None)):
     new_user_ratings = utils.create_new_user_ratings(movieIds, ratings, userId)
     list_of_recommandations = utils.make_recommendations(model, new_user_ratings, userId)
+    list_of_movie_titles = utils.getTitlesFromMovieIds(list_of_recommandations)
     # print(list_of_recommandations)
-    return {"recommendations": list_of_recommandations}
+    return {"recommendations": list_of_movie_titles}
 
 @app.get("/sum")
 def add(q: List[int] = Query(None)):
