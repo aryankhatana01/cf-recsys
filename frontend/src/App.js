66ca20121393ad72650aa6afcc20600c587f8967
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-04 21:24:58 +0530

    created navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import GetRecommendationsButton from './components/getRecommendations/GetRecommendationsButton';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <GetRecommendationsButton />
     </div>
   );
