5199582746f6438a0498cea7f414e7f978db0918
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-05 02:03:48 +0530

    python implementation for levenshtein algo

---
+++ api/levenshtein.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def levenshteinDistanceDP(token1, token2):
+    distances = np.zeros((len(token1) + 1, len(token2) + 1))
+
+    for t1 in range(len(token1) + 1):
+        distances[t1][0] = t1
+
+    for t2 in range(len(token2) + 1):
+        distances[0][t2] = t2
+        
+    printDistances(distances, len(token1), len(token2))
+    return 0
+
+def printDistances(distances, token1Length, token2Length):
+    for t1 in range(token1Length + 1):
+        for t2 in range(token2Length + 1):
+            print(int(distances[t1][t2]), end=" ")
+        print()
+        
+levenshteinDistanceDP("kelm", "hello")
