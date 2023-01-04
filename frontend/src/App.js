336c6933b5bd0390478b6ab3fadec2a6c7185143
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-04 18:49:53 +0530

    added api requests from frontend and displaying them

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [recommendations, setRecommendations] = useState([]);
+
+  const fetchRecommendations = async () => {
+    const response = await fetch('http://localhost:8000/recommend', {
+      method: 'POST',
+      body: JSON.stringify({
+          "movieIds": [
+            5816, 8368
+          ],
+          "ratings": [
+            5, 5
+          ]
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'accept': 'application/json'
+        }
+      });
+    const data = await response.json();
+    setRecommendations(data["recommendations"]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchRecommendations}>Get Recommendations</button>
+      {Object.entries(recommendations).map(([key, value]) => (
+        <div key={key}>{key}: {value}</div>
+      ))}
     </div>
   );
 }
