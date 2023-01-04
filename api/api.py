21e5abafc647ff1865fa21bbe03e04772fbbcb65
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-04 18:49:15 +0530

    fixed cors bug by adding middleware

--- api/api.py
+++ api/api.py
@@ -9,8 +9,22 @@ from typing import Union, List
 from fastapi import FastAPI, Query
 from pydantic import BaseModel
 import utils
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 ### GLOBAL VARIABLES ###
 model_path = '../turimodel/movie_recs.model'
