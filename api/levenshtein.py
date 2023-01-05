49f23f4916696ddb888cecad898d53ebfe54c998
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-05 11:50:08 +0530

    levenshtein algo + docs

--- api/levenshtein.py
+++ api/levenshtein.py
@@ -1,3 +1,7 @@
+"""
+This file contains the implementation of the Levenshtein Distance algorithm.
+"""
+
 import numpy as np
 
 def levenshteinDistanceDP(token1, token2):
@@ -9,13 +13,28 @@ def levenshteinDistanceDP(token1, token2):
     for t2 in range(len(token2) + 1):
         distances[0][t2] = t2
         
-    printDistances(distances, len(token1), len(token2))
-    return 0
+    a = 0
+    b = 0
+    c = 0
+    
+    for t1 in range(1, len(token1) + 1):
+        for t2 in range(1, len(token2) + 1):
+            if (token1[t1-1] == token2[t2-1]):
+                distances[t1][t2] = distances[t1 - 1][t2 - 1]
+            else:
+                a = distances[t1][t2 - 1]
+                b = distances[t1 - 1][t2]
+                c = distances[t1 - 1][t2 - 1]
+                
+                if (a <= b and a <= c):
+                    distances[t1][t2] = a + 1
+                elif (b <= a and b <= c):
+                    distances[t1][t2] = b + 1
+                else:
+                    distances[t1][t2] = c + 1
 
-def printDistances(distances, token1Length, token2Length):
-    for t1 in range(token1Length + 1):
-        for t2 in range(token2Length + 1):
-            print(int(distances[t1][t2]), end=" ")
-        print()
-        
-levenshteinDistanceDP("kelm", "hello")
+    # printDistances(distances, len(token1), len(token2))
+    return distances[len(token1)][len(token2)]
+
+distance = levenshteinDistanceDP("captain", "bruh")
+print(distance)
