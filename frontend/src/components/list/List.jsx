1981f1f3f89f03424d8b1c6dcbd9866003d6aebe
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-06 12:16:05 +0530

    added react router to route pages

---
+++ frontend/src/components/list/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './List.css';
+
+const List = () => {
+    return (
+        <div className="search-bar">
+            <input type="text" placeholder="List" />
+        </div>
+    )
+}
+
+export default List;
---
+++ frontend/src/components/searchbar/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './SearchBar.css';
+
+const SearchBar = () => {
+    return (
+        <div className="search-bar">
+            <input type="text" placeholder="Search" />
+        </div>
+    )
+}
+
+export default SearchBar;
